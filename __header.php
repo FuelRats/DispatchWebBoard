e1191966bae845aa4cb28e29a9f0d659f9a4ac68
UncleClapton <camjwelt@gmail.com>
2016-12-03 05:45:07 +0000

    add and re-arrange header tags.

--- __header.php
+++ __header.php
@@ -1,14 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Fuel Rats - Dispatch Board</title>
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="css/fuel.css" />
-    <link rel="shortcut icon" href="favicon.ico" />
-    <script src="js/jquery.js"></script>
-    <script src="js/bootstrap.js"></script>
-    <script src="js/fr.client.js"></script>
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/app.css" />
+  <link rel="shortcut icon" href="favicon.ico" />
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/fr.client.js"></script>
+  <script src="http://localhost:35729/livereload.js?snipver=1"></script>
 </head>
 <body>
 <?php include("__menu.php"); ?>
-<div class="container">
+<div class="container-fluid">
