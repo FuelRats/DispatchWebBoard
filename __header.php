528c2098724bbb1a3b4ac0bdcf0278da384b3bb7
UncleClapton <camjwelt@gmail.com>
2016-12-07 08:19:38 +0000

    Use minified versions of CSS and JS files. I love minify.

--- __header.php
+++ __header.php
@@ -6,11 +6,11 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Fuel Rats - Dispatch Board</title>
   <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-  <link rel="stylesheet" type="text/css" href="css/app.css" />
+  <link rel="stylesheet" type="text/css" href="css/app.min.css" />
   <link rel="shortcut icon" href="favicon.ico" />
   <script src="js/jquery.min.js"></script>
   <script src="js/bootstrap.min.js"></script>
-  <script src="js/fr.client.js"></script>
+  <script src="js/fr.client.min.js"></script>
   <script src="http://localhost:35729/livereload.js?snipver=1"></script>
 </head>
 <body>
