4400b4d08705b4c4e979079cd1a605c8d28d7175
UncleClapton <camjwelt@gmail.com>
2016-12-07 08:16:34 +0000

    make navbar fluid. Clock should be right-aligned.

--- __menu.php
+++ __menu.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-inverse navbar-fixed-top">
-    <div class="container">
+    <div class="container-fluid">
         <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
                 <span class="sr-only">Toggle navigation</span>
@@ -7,9 +7,9 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#" style="padding: 5px;"><img src="images/fuelrats.png" style="height: 40px; display: inline-block;" /><span id="VersionInfo"></span></a>
+            <a class="navbar-brand" href="#" style="padding: 5px;"><img src="images/fuelrats.png" style="height: 40px; display: inline-block;" /> FuelRats Dispatch Board</a>
         </div>
-        <div id="navbar" class="collapse navbar-collapse">
+        <div id="navbar" class="collapse navbar-collapse navbar-right">
             <ul class="nav navbar-nav">
                 <li><a href="#" class="ed-clock" unselectable="on" onclick="return false;">clock</a></li>
                 <!--<li><a href="/">Rescue list</a></li>
