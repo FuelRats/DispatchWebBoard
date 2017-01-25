a773ad998d41bee45a8073a2cbe395f945f66ed1
UncleClapton <camjwelt@gmail.com>
2017-01-25 20:57:42 +0000

    update file notes.

--- config/fr.config_example.js
+++ config/fr.config_example.js
@@ -1,6 +1,7 @@
 var fr = fr !== undefined ? fr : {};
 var debug = false;
-// IMPORTANT: make sure to rename this file to "fr.config.js"
+// IMPORTANT: make a copy of this file, and name it "fr.config.js"
+// Repo .gitignore ignores "fr.config.js", so updating through git pull is possible.
 fr.config = {
   //Set this to a proper WS Stream URL on setup.
   WebSocketStreamURL: "wss://some.wss.domain.tld:443",
