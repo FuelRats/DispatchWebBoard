310f92d43f306eb42645647e1783c1750508031d
UncleClapton <camjwelt@gmail.com>
2017-03-08 14:21:50 +0000

    separate cookies from the client module. Making them global functions.

--- js/fr.client.js
+++ js/fr.client.js
@@ -26,36 +26,6 @@ fr.client = !fr.config || !fr.ws ? null : {
             if (debug) console.log("fr.client.init - init completed already!");
         }
 	},
-	GetCookie: function(name) {
-        try {
-            var cookie = document.cookie;
-            name = fr.config.CookieBase + name;
-            var valueStart = cookie.indexOf(name + "=") + 1;
-            if (valueStart === 0) {
-                return null;
-            }
-            valueStart += name.length;
-            var valueEnd = cookie.indexOf(";", valueStart);
-            if (valueEnd == -1)
-                valueEnd = cookie.length;
-            return decodeURIComponent(cookie.substring(valueStart, valueEnd));
-        } catch (e) {
-        }
-        return null;
-    },
-    SetCookie: function(name, value, expire) {
-        var temp = fr.config.CookieBase + name + "=" + escape(value) + (expire !== 0 ? "; path=/; expires=" + ((new Date((new Date()).getTime() + expire)).toUTCString()) + ";" : "; path=/;");
-        document.cookie = temp;
-    },
-    CanSetCookies: function() {
-        SetCookie('CookieTest', 'true', 0);
-        var can = GetCookie('CookieTest') !== null;
-        DelCookie('CookieTest');
-        return can;
-    },
-    DelCookie: function(name) {
-        document.cookie = fr.config.CookieBase + name + '=0; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-    },
     HandleTPA: function (tpa) {
         if(debug) console.log("fr.client.HandleTPA - New TPA");
         if(debug) console.log(tpa);
