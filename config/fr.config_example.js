aa209140d2f92803b48a6e8078bdc4a1b7c361b4
UncleClapton <camjwelt@gmail.com>
2017-03-25 23:50:47 +0000

    update config example for new variables.

--- config/fr.config_example.js
+++ config/fr.config_example.js
@@ -3,11 +3,14 @@ var debug = false;
 // IMPORTANT: make a copy of this file, and name it "fr.config.js"
 // Repo .gitignore ignores "fr.config.js", so updating through git pull is possible.
 fr.config = {
-	WebPageTitle: "Page Title",
-  // Board Version
-  VersionInfo: "v0.0",
+  // Title of the page
+  WebPageTitle: "Page Title",
   // Set this to a proper WS Stream URL on setup.
-  WebSocketStreamURL: "wss://some.wss.domain.tld:443",
+  WssURI: "wss://some.wss.domain.tld:443",
+  // FR API location (should be "https://domain.tld/")
+  ApiURI: "https://api.domain.tld/",
+  // FR API ClientID
+  ClientID: "00000000-0000-4000-a000-000000000000",
   // Any preferred prefix. this is to avoid confusion with other copies on the same domain.
   CookieBase: 'cookie_prefix_'
 };
