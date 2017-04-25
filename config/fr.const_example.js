e8382f77e8d69c8f1d5690455de1ebf1c7d92340
Cameron Welter <camjwelt@gmail.com>
2017-04-25 05:28:50 +0000

    move fr.const to it's own config file.

---
+++ config/fr.const_example.js
@@ -0,0 +1,161 @@
+var fr = fr !== undefined ? fr : {};
+
+/* == Below are defined string constants == */
+fr.const = {
+  platform: {
+    'pc': {
+      short: 'PC',
+      long: 'PC'
+    },
+    'xb': {
+      short: 'XB1',
+      long: 'Xbox One'
+    },
+    'ps': {
+      short: 'PS4',
+      long: 'Playstation 4'
+    },
+    'unknown': {
+      short: '?',
+      long: 'Unknown...'
+    }
+  },
+  language: {
+    'bg': {
+      short: 'bg',
+      long: 'Bulgarian'
+    },
+    'bs': {
+      short: 'bs',
+      long: 'Bosnian'
+    },
+    'ca': {
+      short: 'ca',
+      long: 'Catalan / Valencian'
+    },
+    'cs': {
+      short: 'cs',
+      long: 'Czech'
+    },
+    'da': {
+      short: 'da',
+      long: 'Danish'
+    },
+    'de': {
+      short: 'de',
+      long: 'German'
+    },
+    'el': {
+      short: 'el',
+      long: 'Modern Greek'
+    },
+    'en': {
+      short: 'en',
+      long: 'English'
+    },
+    'es': {
+      short: 'es',
+      long: 'Spanish / Castilian'
+    },
+    'et': {
+      short: 'et',
+      long: 'Estonian'
+    },
+    'fi': {
+      short: 'fi',
+      long: 'Finnish'
+    },
+    'fr': {
+      short: 'fr',
+      long: 'French'
+    },
+    'he': {
+      short: 'he',
+      long: 'Modern Hebrew'
+    },
+    'hr': {
+      short: 'hr',
+      long: 'Croatian'
+    },
+    'hu': {
+      short: 'hu',
+      long: 'Hungarian'
+    },
+    'it': {
+      short: 'it',
+      long: 'Italian'
+    },
+    'ja': {
+      short: 'ja',
+      long: 'Japanese'
+    },
+    'lt': {
+      short: 'lt',
+      long: 'Lithuanian'
+    },
+    'lv': {
+      short: 'lv',
+      long: 'Latvian'
+    },
+    'nb': {
+      short: 'nb',
+      long: 'Navajo, Navaho'
+    },
+    'nl': {
+      short: 'nl',
+      long: 'Dutch'
+    },
+    'pl': {
+      short: 'pl',
+      long: 'Polish'
+    },
+    'pt': {
+      short: 'pt',
+      long: 'Portuguese'
+    },
+    'ro': {
+      short: 'ro',
+      long: 'Romanian, Moldavian, Moldovan'
+    },
+    'ru': {
+      short: 'ru',
+      long: 'Russian'
+    },
+    'sk': {
+      short: 'sk',
+      long: 'Slovak'
+    },
+    'sl': {
+      short: 'sl',
+      long: 'Slovene'
+    },
+    'sr': {
+      short: 'sr',
+      long: 'Serbian'
+    },
+    'sv': {
+      short: 'sv',
+      long: 'Swedish'
+    },
+    'th': {
+      short: 'th',
+      long: 'Thai'
+    },
+    'tr': {
+      short: 'tr',
+      long: 'Turkish'
+    },
+    'uk': {
+      short: 'uk',
+      long: 'Ukrainian'
+    },
+    'unknown': {
+      short: '?',
+      long: 'Unknown...'
+    },
+    'zh': {
+      short: 'zh',
+      long: 'Chinese'
+    }
+  }
+};
