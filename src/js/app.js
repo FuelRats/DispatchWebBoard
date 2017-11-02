2c58d1b013f72d65c28bcf96a114ff88419651ba
Cameron Welter <camjwelt@gmail.com>
2017-11-02 11:29:42 +0000

    Add basic test render instruction to test components.

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,70 @@
-import User from './User.js';
+// import User from './User.js';
+// 
+// /* DEVBLOCK:START */
+// import DebugHelper from './util/DebugHelper.js';
+// 
+// window.console.debug = window.console.log.bind(window.console);
+// /* DEVBLOCK:END */
+// 
+// 
+// let dwbApp = new User();
+// 
+// /* DEVBLOCK:START */
+// window._dwbug = new DebugHelper(dwbApp);
+// /* DEVBLOCK:END */
+// 
+// ^^ OLD CODE ABOVE ^^
 
-/* DEVBLOCK:START */
-import DebugHelper from './util/DebugHelper.js';
 
-window.console.debug = window.console.log.bind(window.console);
-/* DEVBLOCK:END */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import RatList from './components/RatList.jsx';
 
 
-let dwbApp = new User();
-
-/* DEVBLOCK:START */
-window._dwbug = new DebugHelper(dwbApp);
-/* DEVBLOCK:END */
+ReactDOM.render(
+  React.createElement(RatList, {
+    rats: [
+      { 
+        'id': '4807dccb-da0c-4244-8483-dab4087e562d',
+        'attributes': { 
+          'name': 'Nepxious', 
+          'platform': 'pc', 
+          'identified': true 
+        } 
+      },
+      { 
+        'id': '3bd62758-bf7d-428b-8711-4f3798e4d2ee',
+        'attributes': { 
+          'name': 'Nepton', 
+          'platform': 'pc', 
+          'identified': true 
+        } 
+      },
+      { 
+        'id': '3e147fbd-3ae2-47b0-936f-d2fff849763b',
+        'attributes': { 
+          'name': 'NoNepKing', 
+          'platform': 'pc', 
+          'identified': true 
+        } 
+      },
+      { 
+        'id': 'add060b6-9333-45c9-b1f8-8153b2a5587c',
+        'attributes': { 
+          'name': 'Nepnaran', 
+          'platform': 'pc', 
+          'identified': true 
+        } 
+      },
+      { 
+        'id': 'e7ed6c61-8ec1-442e-94a4-70ab144ce25e',
+        'attributes': { 
+          'name': 'Nep_Sheets', 
+          'platform': 'pc', 
+          'identified': true 
+        } 
+      },
+    ]
+  }, null),
+  document.getElementById('reactApp')
+);
