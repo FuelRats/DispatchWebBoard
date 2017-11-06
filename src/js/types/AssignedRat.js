1064183a850f09d0ec09da505041d5defb23c442
Cameron Welter <camjwelt@gmail.com>
2017-11-06 00:41:33 +0000

    Expect a data type to be defined.

--- src/js/types/AssignedRat.js
+++ src/js/types/AssignedRat.js
@@ -1,7 +1,9 @@
+// Module imports
 import PropTypes from 'prop-types';
 
 export default PropTypes.shape({
   'id': PropTypes.string.isRequired,
+  'type': PropTypes.oneOf(['assignedRats']).isRequired,
   'attributes': PropTypes.shape({
     'name': PropTypes.string.isRequired,
     'platform': PropTypes.string,
