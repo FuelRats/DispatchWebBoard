ffae40e515b5b2fd4f5c2c14328794bd05b1c5d4
Cameron Welter <camjwelt@gmail.com>
2017-11-06 00:55:59 +0000

    Cleanups and stuff.

--- src/js/util/frConstants.js
+++ src/js/util/frConstants.js
@@ -159,6 +159,20 @@ export const language = {
     long: 'Chinese'
   }
 };
+export const funny = [
+  'iwanttologinplease',
+  'alltheducksareswimminginthewater',
+  'onthefirstdayofratmas',
+  'poweredbymice',
+  'YOURFACE',
+  'claptonbrokeit',
+  'trezyischairmannow',
+  'thedarksoulsofoauthflows',
+  'JOINMEINGLORY',
+  'burymewithmymoney',
+  'whathappensinpopcorn',
+  'evalfixeseverything'
+];
 export const iconSVG = {
   clipboard: '<svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="currentColor" d="M17,9H7V7H17M17,13H7V11H17M14,17H7V15H14M12,3A1,1 0 0,1 13,4A1,1 0 0,1 12,5A1,1 0 0,1 11,4A1,1 0 0,1 12,3M19,3H14.82C14.4,1.84 13.3,1 12,1C10.7,1 9.6,1.84 9.18,3H5A2,2 0 0,0 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5A2,2 0 0,0 19,3Z" /></svg>',
   close:     '<svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="currentColor" d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" /></svg>',
