7cc8a4ae780e9715691ca44cdf1248950118c6b6
Cameron Welter <camjwelt@gmail.com>
2017-11-12 01:53:14 +0000

    add functions to clear hash and query.

--- src/js/Helpers/Url.js
+++ src/js/Helpers/Url.js
@@ -5,9 +5,31 @@
  * @returns {Object}     The value of the specified key, returns null if not found.
  */
 export function getUrlParam(key) {
-  let param = window.location.href.slice(window.location.href.indexOf('?') + 1)
+  let param = window.location.search.slice(1)
     .split('&')
     .find(kvPair => kvPair.split('=')[0] === key);
   return param === undefined ? null : param.split('=')[1];
   // Can you tell I tried REALLY HARD to make this a one liner?
+}
+
+/**
+ * Removes url hash information from the active URL.
+ *
+ * @returns {void}
+ */
+export function clearUrlHash() {
+  if (window.history.replaceState) {
+    window.history.replaceState({}, document.title, window.location.pathname + window.location.search);
+  }
+}
+
+/**
+ * Removes all params defined in the URL.
+ *
+ * @returns {[type]} [description]
+ */
+export function clearUrlParams() {
+  if (window.history.replaceState) {
+    window.history.replaceState({}, document.title, window.location.pathname + window.location.hash);
+  }
 }
