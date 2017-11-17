2e76d24b8f8400d8a2507739fbaa428e173ac07a
Cameron Welter <camjwelt@gmail.com>
2017-11-17 16:34:45 +0000

    Just load the index component on init.

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,6 +1,5 @@
 // App Imports
 import IndexPage from 'Pages/index.jsx';
-import User from 'Classes/User.js';
 
 // Module Imports
 import React from 'react';
@@ -11,34 +10,10 @@ import ReactDOM from 'react-dom';
  *
  * @returns {[type]} [description]
  */
-function init() {
-  let user = new User();
-
-  // Begin user authentication process.
-  if (user.hasToken()) {
-    user.authenticate().then(() =>{
-      if (user.hasPermission()) {
-        // Begin Client
-        
-        let page = (<IndexPage user={user} />);
-        ReactDOM.render(page, document.getElementById('reactApp'));
+function init() {       
+  let page = (<IndexPage />);
+  ReactDOM.render(page, document.getElementById('reactApp'));
   
-  
-        window.console.log(`%cWelcome CMDR ${user.getUserDisplayName()}. We're now on v1.0! Fly safe!`,
-          'color: lightgreen; font-weight: bold; font-size: 1.25em;');
-  
-        window._dwbug = {
-          'UserCon': user,
-          'page': page
-        };
-      } else {
-        // Show "user must be drilled" page
-      }
-    }).catch(() => {
-      // Display login.
-    });
-  } else {
-    // Display login. (or in this case just head to the login page for ease.)
-  }
+  window._dwbug = page;
 }
 init();
