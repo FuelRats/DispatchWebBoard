b81647e600125cb98e3e3aa42324d66404896aee
Cameron Welter <camjwelt@gmail.com>
2017-11-19 02:14:09 +0000

    Add User and theme loading.

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,19 +1,38 @@
 // App Imports
+import User from 'Classes/User.js';
 import IndexPage from 'Pages/index.jsx';
 
 // Module Imports
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+
+export let CurrentUser = null;
+
 /**
  * Handles user authentication and app injection.
  *
  * @returns {[type]} [description]
  */
-function init() {       
+function init() {
+  CurrentUser = new User();
+
+  // Handle theming and changes to the theme setting.
+  CurrentUser.Store.observe('boardTheme', (ctx, newValue) => {
+    document.getElementsByTagName('body')[0].className = `theme-${newValue}`;
+  });
+  document.getElementsByTagName('body')[0].className = `theme-${CurrentUser.Store.boardTheme}`;
+
+  // Render page.
   let page = (<IndexPage />);
   ReactDOM.render(page, document.getElementById('reactApp'));
   
-  window._dwbug = page;
+  // Expose user and page for console access during debug
+  window._dwbug = {
+    'user': CurrentUser,
+    'page': page
+  };
 }
+
 init();
