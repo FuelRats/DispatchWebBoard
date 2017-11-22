146a428841e03fc980e6208491d6fb2061a6370d
Cameron Welter <camjwelt@gmail.com>
2017-11-22 21:20:59 +0000

    Refactor lStore and sStore to WebStore.(local|session)

--- src/js/Helpers/Storage.js
+++ src/js/Helpers/Storage.js
@@ -1,13 +1,14 @@
 import AppConfig from 'Config/Config.js';
 
-export const lStore = {
-  get: (key) => localStorage.getItem(`${AppConfig.AppNamespace}.${key}`),
-  set: (key, value) => localStorage.setItem(`${AppConfig.AppNamespace}.${key}`, value),
-  remove: (key) => localStorage.removeItem(`${AppConfig.AppNamespace}.${key}`)
-};
-
-export const sStore = {
-  get: (key) => sessionStorage.getItem(`${AppConfig.AppNamespace}.${key}`),
-  set: (key, value) => sessionStorage.setItem(`${AppConfig.AppNamespace}.${key}`, value),
-  remove: (key) => sessionStorage.removeItem(`${AppConfig.AppNamespace}.${key}`)
+export const WebStore = {
+  local: {
+    get: (key) => localStorage.getItem(`${AppConfig.AppNamespace}.${key}`),
+    set: (key, value) => localStorage.setItem(`${AppConfig.AppNamespace}.${key}`, value),
+    remove: (key) => localStorage.removeItem(`${AppConfig.AppNamespace}.${key}`)
+  },
+  session: {
+    get: (key) => sessionStorage.getItem(`${AppConfig.AppNamespace}.${key}`),
+    set: (key, value) => sessionStorage.setItem(`${AppConfig.AppNamespace}.${key}`, value),
+    remove: (key) => sessionStorage.removeItem(`${AppConfig.AppNamespace}.${key}`)
+  }
 };
