8c8fb7afc7bc49298aec3e9600f4e6338d90abec
Cameron Welter <camjwelt@gmail.com>
2017-11-22 21:32:31 +0000

    Update setting file.

--- src/js/Config/DefaultSettings.js
+++ src/js/Config/DefaultSettings.js
@@ -1,11 +1,15 @@
 export default {
+  'useWG': {
+    'value': false
+  },
   'boardTheme': {
     'value': 'dark',
-    'options': [
-      'dark',
-      'light',
-      'twitchPurple',
-      'wireframe'
-    ]
+    'options': {
+      'dark': 'Dark',
+      'light': 'Light',
+      'amber': 'Elite Amber',
+      'twitchPurple': 'Twitch Purple',
+      'wireframe' : 'debug_wireframe',
+    }
   }
 };
