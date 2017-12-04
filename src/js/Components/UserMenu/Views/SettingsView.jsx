2d5fc7b96f2590c24a8b69ad798e19fbd96200c8
Cameron Welter <camjwelt@gmail.com>
2017-12-04 14:57:47 +0000

    Implement basic settings just so we have SOMETHING there.

--- src/js/Components/UserMenu/Views/SettingsView.jsx
+++ src/js/Components/UserMenu/Views/SettingsView.jsx
@@ -1,5 +1,8 @@
 // App Imports
 import Component from 'Components/Component.jsx';
+import { CurrentUser } from 'app.jsx';
+import { SETTING_KEYS as SETTINGS } from 'Config/DefaultSettings.js';
+
 
 // Module Imports
 import React from 'react';
@@ -18,6 +21,53 @@ export default class SettingsView extends Component {
    */
   constructor(props) {
     super(props);
+
+    this._bindMethods([
+      'handleSettingChange',
+      'handleSettingsClose'
+    ]);
+  }
+
+  /**
+   * Handles when a user setting is changed.
+   *
+   * @param {Object} event Event 
+   * @returns {void}
+   */
+  handleSettingChange(event) {
+    let {
+      target
+    } = event;
+
+    window.console.debug(event.target.type);
+
+    let 
+      changedSetting = target.name || null,
+      newValue = null;
+
+    if (event.target.type === 'checkbox') {
+      newValue = target.checked;
+    } else {
+      newValue = target.value;
+    }
+
+
+    if (changedSetting) {
+      window.console.debug('setting: ', changedSetting, newValue);
+      let result = CurrentUser.store.set(changedSetting, newValue);
+      window.console.debug(result);
+    }
+  }
+
+  /**
+   * Saves the new settings and closes the settings dialog.
+   *
+   * @returns {void}
+   */
+  handleSettingsClose() {
+    CurrentUser.store.save();
+
+    this.props.viewChangeHandler('menu');
   }
 
   /**
@@ -26,9 +76,35 @@ export default class SettingsView extends Component {
    * @returns {Object} React element.
    */
   render() {
+
+
+    let useWing = (
+      <label className={`setting-${SETTINGS.USE_WG}`}>Use &quot;WG&quot; for wing:
+        <input name={SETTINGS.USE_WG} onChange={this.handleSettingChange} defaultChecked={CurrentUser.store.useWG} type="checkbox"></input>
+      </label>
+    );
+
+
+    let themeOptions = CurrentUser.store.getOptionsWithHumanReadable(SETTINGS.BOARD_THEME)
+      .map(([ setting, description ]) => <option key={setting} value={setting}>{description}</option>);
+    let boardTheme = (
+      <label className={`setting-${SETTINGS.BOARD_THEME}`}>Theme: 
+        <select name={SETTINGS.BOARD_THEME} defaultValue={CurrentUser.store.boardTheme} onChange={this.handleSettingChange}>
+          {themeOptions}
+        </select>
+      </label>
+    );
+
+
     return (
       <div className='user-settings-view'>
-        <span>DUMMY DUMMY DUMMY</span>
+        <div className='user-view-controls'>
+          <button className='button button-small button-close' onClick={this.handleSettingsClose}>Save & Close</button>
+        </div>
+        <div className='user-options'>
+          {useWing}
+          {boardTheme}
+        </div>
       </div>
     );
   }
