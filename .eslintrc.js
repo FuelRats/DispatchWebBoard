4086cf2d6e8a79e1bc40fa4ec5f5b0acfaa46d84
Cameron Welter <camjwelt@gmail.com>
2020-02-29 21:04:02 +0000

    turn off dat jsdoc requirement yo

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,11 +6,12 @@ module.exports = {
   },
   extends: '@fuelrats/eslint-config',
   parserOptions: {
-    "ecmaVersion": 2018,
+    "ecmaVersion": 2020,
     "sourceType": "module",
   },
   rules: {
     complexity: ['off'], // Disabled cuz we're not interested in fixing complexity problems with the old board.
     'prefer-promise-reject-errors': ['off'], // because of code to be fixed later
+    'jsdoc/require-jsdoc': ['off'],
   }
 }
